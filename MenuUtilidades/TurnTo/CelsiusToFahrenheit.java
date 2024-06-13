a5bed280c1e9d7eed7b23f638d7633df63f2b60d
Daniel Cipriani <danielcipriani321@gmail.com>
2024-06-12 22:25:38 -0300

    Retirado e separado menu de utilidades

--- MenuUtilidades/TurnTo/CelsiusToFahrenheit.java
+++
@@ -1,9 +0,0 @@
-package MenuUtilidades.TurnTo;
-
-public class CelsiusToFahrenheit extends TurnBase{
-
-    public CelsiusToFahrenheit(){
-        double result = number * 1.8 + 32;
-        System.out.println(result);
-    }
-}
--- MenuUtilidades/TurnTo/FahreinheitToCelsius.java
+++
@@ -1,9 +0,0 @@
-package MenuUtilidades.TurnTo;
-
-public class FahreinheitToCelsius extends TurnBase{
-
-    public FahreinheitToCelsius(){
-        double result = (number - 32) / 1.8;
-        System.out.println(result);
-    }
-}
